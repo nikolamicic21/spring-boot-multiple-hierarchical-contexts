1f3ce500ee9b45f056a588a9afe9ed1c3d29062f
Nikola Micic <mickeandroid21@gmail.com>
2021-09-17 00:03:48 +0200

    Multiple contexts where WebService bean can access DataService bean

--- src/main/java/io/nikolamicic21/multiplehierarchicalcontexts/data/DataService.java
+++ src/main/java/io/nikolamicic21/multiplehierarchicalcontexts/data/DataService.java
@@ -1,14 +1,9 @@
 package io.nikolamicic21.multiplehierarchicalcontexts.data;
 
-import io.nikolamicic21.multiplehierarchicalcontexts.web.WebService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class DataService {
-
-    private final WebService webService;
 
     public String getName() {
         return getClass().getSimpleName() + "#" + getClass().getMethods()[0].getName();
