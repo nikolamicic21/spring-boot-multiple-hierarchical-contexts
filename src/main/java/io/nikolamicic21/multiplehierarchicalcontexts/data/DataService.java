52488f9918b91dab3480cafe30babdb8c149ce93
Nikola Micic <mickeandroid21@gmail.com>
2021-09-16 23:48:23 +0200

    Update DataService to depend on WebService
    Removing WebService dependency on DataService.

--- src/main/java/io/nikolamicic21/multiplehierarchicalcontexts/data/DataService.java
+++ src/main/java/io/nikolamicic21/multiplehierarchicalcontexts/data/DataService.java
@@ -1,12 +1,16 @@
 package io.nikolamicic21.multiplehierarchicalcontexts.data;
 
+import io.nikolamicic21.multiplehierarchicalcontexts.web.WebService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DataService {
+
+    private WebService webService;
 
     public String getName() {
         return getClass().getSimpleName() + "#" + getClass().getMethods()[0].getName();
     }
-
 }
--- src/main/java/io/nikolamicic21/multiplehierarchicalcontexts/web/WebService.java
+++ src/main/java/io/nikolamicic21/multiplehierarchicalcontexts/web/WebService.java
@@ -1,17 +1,11 @@
 package io.nikolamicic21.multiplehierarchicalcontexts.web;
 
-import io.nikolamicic21.multiplehierarchicalcontexts.data.DataService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class WebService {
-
-    private final DataService dataService;
 
     public String getName() {
         return getClass().getSimpleName() + "#" + getClass().getMethods()[0].getName();
     }
-
 }
